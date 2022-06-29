9d652d2260f2cc5dec9179e021e4274071a75497
artemis <arthur.toma@gmail.com>
2022-06-28 21:37:02 -0400

    Added css styling and button hover function. removed html onclick and made js buton event listeners

--- script.js
+++ script.js
@@ -9,68 +9,112 @@ let createGrid= (gridSize) =>{
         container.appendChild(grid);
         
         /*event listener when  mouse is pressed*/
-        grid.addEventListener('mousedown', () => {mouseDown = 'true'});
+/*      grid.addEventListener('mousedown', () => {mouseDown = 'true'});
         grid.addEventListener('mouseup', () => {mouseDown = 'false'})
-
+*/
         container.setAttribute('style', 
         `grid-template-columns: repeat(${gridSize}, 1fr);grid-template-rows: repeat(${gridSize}, 1fr)`)
+
     }
 }
 
 /*creates random color*/
 let randomColor = () =>{
-    return Math.floor(Math.random()*1677215).toString(16).padEnd(6,2);
+    return Math.floor(Math.random()*1677215).toString(16).padStart(6,'f');
 }
 
-console.log(randomColor())
-let rainbow= 'false';
-let Rainbow=() => {rainbow= 'true';};
-let BlackWhite = () => {rainbow= 'false'};
+let multiColor= 'false';
 
-/*clears formatting */
-let ClearFormating= () => {
+/**Rainbow button */
+const rainbowBtn = document.querySelector('#rainbow');
+rainbowBtn.addEventListener('mouseover', ()=>{
+    rainbowBtn.style.backgroundColor = 'paleturquoise'
+})
+rainbowBtn.addEventListener('mouseout', ()=> {
+    rainbowBtn.style.backgroundColor='palegoldenrod'
+})
+rainbowBtn.addEventListener('click', ()=>{
+     multiColor='true';
+})
+
+/**Black and white button */
+const blackWhiteBtn = document.querySelector('#blackWhite');
+blackWhiteBtn.addEventListener('mouseover', ()=>{
+    blackWhiteBtn.style.backgroundColor = 'paleturquoise'
+})
+blackWhiteBtn.addEventListener('mouseout', ()=> {
+    blackWhiteBtn.style.backgroundColor='palegoldenrod'
+})
+blackWhiteBtn.addEventListener('click', ()=>{
+     multiColor='false';
+})
+
+/*clears grid*/
+let ClearGrid= () => {
     document.querySelectorAll('#gridItem')
         .forEach((e) => {
             e.style.backgroundColor = 'white';
         })
 }
 
+/**Reset button */
+const resetBtn =document.querySelector('#clearGrid');
+resetBtn.addEventListener('mouseover', ()=>{
+    resetBtn.style.backgroundColor = 'paleturquoise'
+})
+resetBtn.addEventListener('mouseout', ()=> {
+    resetBtn.style.backgroundColor='palegoldenrod'
+})
+resetBtn.addEventListener('click', ()=>{
+    ClearGrid();
+})
+
+/**Grid size button */
+const gridPixel = document.querySelector('#gridPixel');
+gridPixel.addEventListener('click', () => {
+    reset();
+    gridSize = prompt('Input grid size');
+    while (gridSize<16 || gridSize>64){
+        gridSize = prompt('Please input a number between 16 and 64.');
+    }
+    createGrid(gridSize);
+    hover();   
+}) 
+
+gridPixel.addEventListener('mouseover', ()=>{
+    gridPixel.style.backgroundColor = 'paleturquoise'
+})
+gridPixel.addEventListener('mouseout', ()=> {
+    gridPixel.style.backgroundColor='palegoldenrod'
+})
+
+
 /*turn grid square black when mouse is over*/
 let hover = () => {
     document.querySelectorAll('#gridItem')
         .forEach((e) =>{
         e.addEventListener('mouseover', () => {
-            if (rainbow == 'false'){
+            if (multiColor == 'false'){
                 e.style.backgroundColor = 'black';
-            } else if ( rainbow == 'true') {
+            } else if ( multiColor == 'true') {
                 color = '#' + randomColor();
-                console.log(color);
                 e.style.backgroundColor = `${color}`; 
             
             }
         }); 
     });       
 }
+
 /*function to clear Grid*/
 let reset = () => {
     document.querySelectorAll('#gridItem')
-    .forEach((e) => e.parentNode.removeChild(e));
-    
+    .forEach((e) => e.parentNode.removeChild(e));   
 }
 
 /*default grid size*/
 createGrid(16);
 hover()
 
-/*select grid size*/
-const gridBtn = document.querySelector('#gridPixel');
-gridBtn.addEventListener('click', () => {
-    reset();
-    gridSize = prompt('Input grid size');
-    createGrid(gridSize);
-    hover();
-    
-}) 
 
 
 
